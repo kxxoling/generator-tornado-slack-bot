d08ed241b2f5e928e58d1903595a6664f461cdd2
kxxoling <kxxoling@gmail.com>
2016-08-13 20:55:59 +0800

    Distinguish dot files with source files.

--- app/index.js
+++ app/index.js
@@ -2,6 +2,11 @@ const generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
   app() {
-    this.directory('./', './');
+    this.directory(
+      this.templatePath('src'),
+      './'
+    );
+    this.copy(this.templatePath('dotfiles', '_editorconfig'), '.editorconfig');
+    this.copy(this.templatePath('dotfiles', '_gitignore'), '.gitignore');
   },
 });
